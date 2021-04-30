2317879e0f1c63ba422f01ee5da2ce99e921372e
Jari Yuen <kashing.yuen.5j@stu.hosei.ac.jp>
2021-04-30 18:18:54 +0800

    init

---
+++ memo/models.py
@@ -0,0 +1,19 @@
+# from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+class Memo(models.Model):
+    class Meta:
+        verbose_name = 'Memo Table'
+        verbose_name_plural = 'Memos Table'
+        db_table = 'memo'
+
+    content = models.CharField(verbose_name='メモ', max_length=100, default='', blank=True,null=True)
+    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
+    
+    def __str__(self):
+        return self.content + '(' + datetime(self.created_at).strftime('%Y/%m/%d %H:%M:%S') + ')'
---
+++ memo/views/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the memo’s index.")
+
+class Home(generic.TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super(Home, self).get_context_data()
+        context.update({'my_message': 'Welcome to my site'})
+        return render(request, 'home.html', context)
